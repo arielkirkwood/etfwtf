d8334d2254698ed209cff143e2664ba66a19d02d
Ariel Kirkwood <ariel.kirkwood@me.com>
2024-03-24 22:30:19 -0400

    Comment out debugger

--- lib/holdings/extractor.rb
+++ lib/holdings/extractor.rb
@@ -18,8 +18,8 @@ module Holdings
 
         fund.save! if holdings.any?
         Rails.logger.info("#{fund.name} saved, holdings: #{holdings.count}")
-      rescue ActiveRecord::RecordInvalid => e
-        debugger
+      # rescue ActiveRecord::RecordInvalid => e
+      #   debugger
       end
     end
 
