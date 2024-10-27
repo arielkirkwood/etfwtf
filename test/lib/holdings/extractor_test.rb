c79c40c298784a0912f4f4372559281ab0686e17
Ariel Kirkwood <ariel.kirkwood@me.com>
2024-10-27 14:26:43 -0400

    Initial coverage for `attach_holdings_file`

--- test/lib/holdings/extractor_test.rb
+++ test/lib/holdings/extractor_test.rb
@@ -6,11 +6,24 @@ class ExtractorTest < ActiveSupport::TestCase
   setup do
     @fund = funds(:crbn)
     @extractor = Holdings::Extractor.new(@fund)
+    @portfolio = @extractor.portfolio
   end
 
   test 'initialize' do
     assert_equal @extractor.fund, @fund
-    assert_equal @extractor.portfolio, @fund.latest_portfolio
+    assert_not @portfolio.persisted?
+  end
+
+  test 'attach holdings file' do
+    assert_not @portfolio.holdings_file.attached?
+
+    VCR.use_cassette('ishares_holdings_file') do
+      @extractor.attach_holdings_file
+    end
+
+    assert @portfolio.holdings_file.attached?
+    assert_equal @portfolio.date, Time.zone.today
+    assert_equal @portfolio, @fund.latest_portfolio
     assert_equal @fund.portfolios.count, 1
   end
 end
